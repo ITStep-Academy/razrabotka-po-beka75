3c869ee368ee884e7c86f7971689e122eff29789
beka75 <69244945+beka75@users.noreply.github.com>
2020-08-05 17:14:32 +0500

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -5,15 +5,13 @@ using namespace std;
 
 int main()
 {
-	long d, h, m, s;
+	long h, m, s;
 	cin >> s;
-	d = s / 86400;
-	s -= d * 86400;
 	h = s / 3200;
 	s -= h * 3200;
 	m = s / 60;
 	s -= m * 60;
-	cout << "Days: " << d << "\nHours: " << h << "\nMinutes: " << m << "\nSeconds: " << s << endl;
+	cout << "\nHours: " << h << "\nMinutes: " << m << "\nSeconds: " << s << endl;
 	_getch();
 	return 0;
 }
